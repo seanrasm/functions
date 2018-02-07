22dd9277505ac2273bb8e4b74db1088f9021964c
seanrasm <35642475+seanrasm@users.noreply.github.com>
2018-02-06 20:59:11 -0600

    mod6_lab

---
+++ stringReverse.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+      $name = "Sebastion";
+    ?>
+  <title><||Loop||></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="index.html" class="navbar-brand">FUNCTION</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="profilePage.html">LOGIN</a></li>
+        <li><a href="aboutEther.html">ABOUT</a></li>
+        <li><a href="signup.html">REQUEST</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid bg-1 text-center">
+  <h1>Function</h1>
+  <img src="loopimage.jpeg">
+<div class="container text-center">
+  <h2 class="wordSpace">Lab</h2>
+  <br>
+  <blockquote>
+    <?php
+    function reverse($string)
+{
+    if (strlen($string) < 2) {
+        return $string;
+    }
+
+    $middle = (int) strlen($string) / 2;
+    $left = substr($string, 0, $middle);
+    $right = substr($string, $middle);
+
+    $rev = reverse($right) . reverse($left);
+    echo $rev;
+}
+	reverse($name);
+	
+  ?>
+
+ </blockquote>
+</div>
+<div class="container-fluid bg-2 text-center">
+<!--
+    <div class="space">
+    <h4><a href="https://mail.protonmail.com/login">Secure Mail</a></h4>
+    <br>
+    <h4><a href="https://slack.com/downloads/osx">Message Service</a></h4>
+    <br> -->
+
+  <h3 class="bottomBye">Function</h3>
+
+</div>
+<br>
+<h4 class="contact contactReduce"><a href="mailto:deathTheKid@protonmail.com?subject=Hola
+&body=Please grant me the wisdom to loop!">Contact</a></h4>
+
+
+<!-- footer remover {css bg.4} -->
+<footer class="container-fluid space bg-4 text-center">
+  <p>Remember To <a href="https://motherboard.vice.com/en_us/article/59zdjq/an-idiots-guide-to-building-an-ethereum-mining-rig">Learn</a></p>
+</footer>
+
+</body>
+</html>
